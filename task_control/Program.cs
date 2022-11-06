848268b16fac0e872de64ce5b5c7eaa05c54bd21
Artyom Prudov <artyom1911@gmail.com>
2022-11-06 22:41:24 +0300

    Del com

--- task_control/Program.cs
+++ task_control/Program.cs
@@ -1,10 +1,4 @@
-﻿// // // ввод 3 строк
-// // массив из 3 строк
-// // подсчет в масиве колличество симоволов
-// // создать массив с кол-вом символов мень 3
-
-
-string[] stringArray = new string[3];
+﻿string[] stringArray = new string[3];
 int j = 0; 
 int size = 0;
 for(int i = 0; i < stringArray.Length; i++)
@@ -37,7 +31,7 @@ Console.WriteLine("\nМассив: {0}",String.Join(" ",newStringArray));
 
 
 
-// Console.Write(size);
+
 
 
 
